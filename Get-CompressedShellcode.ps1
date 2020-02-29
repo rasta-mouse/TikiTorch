07a8b9dcb6dc2ae6217590e125ac35a3073a05f8
NotoriousRebel <matt12299@yahoo.com>
2020-02-29 11:59:34 -0500

    Formatted script.

--- Get-CompressedShellcode.ps1
+++ Get-CompressedShellcode.ps1
@@ -1,29 +1,29 @@
 function Get-CompressedShellcode {
 
-	[CmdletBinding()]        
+    [CmdletBinding()]        
     Param
-   (
-    [parameter(Mandatory=$true)]
-    [String]
-    $inFile,
-    [parameter(Mandatory=$false)]
-    [int]$encoding
-   )
+    (
+        [parameter(Mandatory = $true)]
+        [String]
+        $inFile,
+        [parameter(Mandatory = $false)]
+        [int]$encoding
+    )
   
-   $byteArray = [System.IO.File]::ReadAllBytes($inFile)
-   [System.IO.MemoryStream] $output = New-Object System.IO.MemoryStream
-   $gzipStream = New-Object System.IO.Compression.GzipStream $output, ([IO.Compression.CompressionMode]::Compress)
-   $gzipStream.Write($byteArray, 0, $byteArray.Length)
-   $gzipStream.Close()
-   $output.Close()
-   $tmp = $output.ToArray()
+    $byteArray = [System.IO.File]::ReadAllBytes($inFile)
+    [System.IO.MemoryStream] $output = New-Object System.IO.MemoryStream
+    $gzipStream = New-Object System.IO.Compression.GzipStream $output, ([IO.Compression.CompressionMode]::Compress)
+    $gzipStream.Write($byteArray, 0, $byteArray.Length)
+    $gzipStream.Close()
+    $output.Close()
+    $tmp = $output.ToArray()
 
-    if($encoding -eq 1 -or $encoding -eq $false){
+    if ($encoding -eq 1 -or $encoding -eq $false) {
         $b64 = [System.Convert]::ToBase64String($tmp)
         Write-Output $b64
     }
-    else{
-        $hex = ($tmp | Format-Hex | Select-Object -Expand Bytes | ForEach-Object { '{0:X2}' -f $_ } | ForEach-Object { $i = 0 } { $i++; @{$true=" ";$false=""}[($i - 1) % 16 -eq 0] + $_ }) -join ' '
+    else {
+        $hex = ($tmp | Format-Hex | Select-Object -Expand Bytes | ForEach-Object { '{0:X2}' -f $_ } | ForEach-Object { $i = 0 } { $i++; @{$true = " "; $false = "" }[($i - 1) % 16 -eq 0] + $_ }) -join ' '
         Write-Output $hex.replace("  ", " ")
     }   
 }
