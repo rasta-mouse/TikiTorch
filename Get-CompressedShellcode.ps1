aed097837a3a6c0bc7b09d88e0d5db68998381b9
NotoriousRebel <matt12299@yahoo.com>
2020-02-29 11:53:56 -0500

    Updated Get-CompressedShellcode to support conversion of shell code to hex.

--- Get-CompressedShellcode.ps1
+++ Get-CompressedShellcode.ps1
@@ -1,18 +1,30 @@
 function Get-CompressedShellcode {
 
-	[CmdletBinding()]
-    Param(
-    [String]$inFile
-    )
+	[CmdletBinding()]        
+    Param
+   (
+    [parameter(Mandatory=$true)]
+    [String]
+    $inFile,
+    [parameter(Mandatory=$false)]
+    [int]$encoding
+   )
+  
+   $byteArray = [System.IO.File]::ReadAllBytes($inFile)
+   [System.IO.MemoryStream] $output = New-Object System.IO.MemoryStream
+   $gzipStream = New-Object System.IO.Compression.GzipStream $output, ([IO.Compression.CompressionMode]::Compress)
+   $gzipStream.Write($byteArray, 0, $byteArray.Length)
+   $gzipStream.Close()
+   $output.Close()
+   $tmp = $output.ToArray()
 
-    $byteArray = [System.IO.File]::ReadAllBytes($inFile)
-    [System.IO.MemoryStream] $output = New-Object System.IO.MemoryStream
-    $gzipStream = New-Object System.IO.Compression.GzipStream $output, ([IO.Compression.CompressionMode]::Compress)
-    $gzipStream.Write($byteArray, 0, $byteArray.Length)
-    $gzipStream.Close()
-    $output.Close()
-    $tmp = $output.ToArray()
-    
-    $b64 = [System.Convert]::ToBase64String($tmp)
-    Write-Output $b64
+    if($encoding -eq 1 -or $encoding -eq $false){
+        $b64 = [System.Convert]::ToBase64String($tmp)
+        Write-Output $b64
+    }
+    else{
+        $hex = ($tmp | Format-Hex | Select-Object -Expand Bytes | ForEach-Object { '{0:X2}' -f $_ } | ForEach-Object { $i = 0 } { $i++; @{$true=" ";$false=""}[($i - 1) % 16 -eq 0] + $_ }) -join ' '
+        Write-Output $hex.replace("  ", " ")
+    }   
 }
+
